12629a55de3ae7eda436a4b1d132aa595480ebe5
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-02-21 19:46:53 +0800

    Extend keybindings for telescope builtins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,25 +5,13 @@ return {
 	keys = function()
 		local builtin = require("telescope.builtin")
 		return {
-			{ "<leader>ff", function() builtin.find_files({ no_ignore = true }) end },
-			{ "<leader>fg", function() builtin.live_grep() end},
+			{ "<leader>ff", function() builtin.find_files() end },
+			{ "<leader>fF", function() builtin.find_files({ cwd = vim.fn.input("path: ", "", "file") }) end },
+			{ "<leader>fg", function() builtin.live_grep() end },
 			{ "<leader>fG", function() builtin.live_grep({ grep_open_files = true }) end },
 			{ "<leader>fb", function() builtin.buffers() end },
+			{ "<leader>fq", function() builtin.quickfix() end },
 		}
 	end,
-	opts = {
-		defaults = {
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-				"--hidden",
-				"--no-ignore"
-			}
-		}
-	}
+	opts = {},
 }
