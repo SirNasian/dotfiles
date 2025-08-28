46403418f6de156b544f0df984570759777e4e2f
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-08-28 15:40:34 +0800

    adjust slop commands to use opengl

--- screenrecord.sh
+++ screenrecord.sh
@@ -2,7 +2,7 @@
 if [ -e /tmp/screenrecord.pid ]; then
 	kill $(cat /tmp/screenrecord.pid)
 else
-	read x y w h <<< $(slop -of "%x %y %w %h")
+	read x y w h <<< $(slop -lc 1,1,1,0.5 -f "%x %y %w %h")
 	echo $x $y $w $h
 	ffmpeg -video_size "${w}x${h}" -framerate 24 -f x11grab -i ":0.0+${x},${y}" /tmp/screenrecord.mp4 &
 	notify-send "Screen Recording" "Recording started"
--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-scrot -a $(slop -of "%x,%y,%w,%h") -F /tmp/screenshot
+scrot -a $(slop -lc 1,1,1,0.5 -f "%x,%y,%w,%h") -F /tmp/screenshot
 xclip -selection clipboard -t image/png -i /tmp/screenshot
 rm /tmp/screenshot
