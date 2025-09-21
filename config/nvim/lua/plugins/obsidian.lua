c8f17e4a2270889dcd4d15f70521674ac958b8b3
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-09-21 18:50:45 +0800

    update neovim plugin from `epwalsh/obsidian.nvim` to `obsidian-nvim/obsidian.nvim`

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -1,19 +1,9 @@
 return {
-	"epwalsh/obsidian.nvim",
-	keys = {
-		{ "<leader>nos", function() vim.cmd("ObsidianQuickSwitch") end },
-		{ "<leader>noj", function() vim.cmd("ObsidianDailies") end },
-		{ "<leader>non", function() vim.cmd("ObsidianNew") end },
-		{ "<leader>no/", function() vim.cmd("ObsidianSearch") end },
-		{ "<leader>now", function() vim.cmd("ObsidianWorkspace") end },
-		{ "<leader>nob", function() vim.cmd("ObsidianBacklinks") end },
-		{ "<leader>not", function() vim.cmd("ObsidianTags") end },
-	},
+	"obsidian-nvim/obsidian.nvim",
+	version = "*",
+	ft = "markdown",
 	opts = {
-		workspaces = { { name = "notes", path = "~/.obsidian/notes" } },
-		daily_notes = { folder = "Daily" },
-		follow_url_func = function (url) vim.fn.jobstart({ "xdg-open", url }) end,
-		note_id_func = function(title) return title end,
+		workspaces = {},
 		disable_frontmatter = true,
 	},
 }
