280dad77e15700cc8685e7d88a54bffb3b4df6de
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2026-02-26 22:58:08 +0800

    remove unnecessary configuration for `nvim-treesitter`

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = function()
-		require("nvim-treesitter.config").setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	lazy = false,
 }
