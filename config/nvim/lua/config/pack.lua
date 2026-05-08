9d9b318977e4ffb6231aadb958ba19eda8ab40fb
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2026-05-08 18:53:20 +0800

    add `mini.nvim`

--- config/nvim/lua/config/pack.lua
+++ config/nvim/lua/config/pack.lua
@@ -4,6 +4,7 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" }, -- required by codecompanion & telescope
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim"},
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- required by lualine
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }, -- required by codecompanion
@@ -17,25 +18,12 @@ vim.pack.add({
 
 require("lualine").setup()
 require("oil").setup()
-
-require("toggleterm").setup({
-	open_mapping = [[<leader><leader>]],
-	direction = "float",
-	insert_mappings = false,
-	terminal_mappings = false,
-})
-
-require("telescope").setup()
-local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope.find_files)
-vim.keymap.set("n", "<leader>fg", telescope.live_grep)
-vim.keymap.set("n", "<leader>fb", telescope.buffers)
-vim.keymap.set("n", "<leader>fq", telescope.quickfix)
-vim.keymap.set("n", "<leader>fF", function() telescope.find_files({ cwd = vim.fn.input("path: ", "", "file") }) end)
-vim.keymap.set("n", "<leader>fG", function() telescope.live_grep({ grep_open_files = true }) end)
+require("mini.icons").setup()
+require("mini.notify").setup()
 
 require("codecompanion").setup({
 	interactions = {
+		chat = { adapter = "claude_code" },
 		cli = {
 			agent = "claude_code",
 			agents = {
@@ -47,7 +35,35 @@ require("codecompanion").setup({
 				},
 			},
 		},
-		chat = { adapter = "claude_code" },
-		inline = { adapter = "copilot" },
 	},
+})
+
+require("mini.pick").setup({
+	window = {
+		config = function()
+			local height = math.floor(0.618 * vim.o.lines)
+			local width = math.floor(0.618 * vim.o.columns)
+			return {
+				anchor = "NW", height = height, width = width,
+				row = math.floor(0.5 * (vim.o.lines - height)),
+				col = math.floor(0.5 * (vim.o.columns - width)),
+			}
+		end,
+	},
+})
+
+require("telescope").setup()
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
+vim.keymap.set("n", "<leader>fq", telescope.quickfix)
+vim.keymap.set("n", "<leader>fF", function() telescope.find_files({ cwd = vim.fn.input("path: ", "", "file") }) end)
+vim.keymap.set("n", "<leader>fG", function() telescope.live_grep({ grep_open_files = true }) end)
+
+require("toggleterm").setup({
+	open_mapping = [[<leader><leader>]],
+	direction = "float",
+	insert_mappings = false,
+	terminal_mappings = false,
 })
